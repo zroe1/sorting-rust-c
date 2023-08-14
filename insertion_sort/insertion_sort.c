0cb1a0a4e275c8e454e9512198f7e04198e83789
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-08-14 18:02:30 -0500

    changed arr range from 0 to 65,535 in insertion_sort.c

---
+++ insertion_sort/insertion_sort.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+unsigned int ARRAY_LENGTH = 10;
+
+/* checks if a given array is sorted */
+int check_sorted(unsigned int *nums, unsigned int nums_len) {
+  for (unsigned int i = 0; i < nums_len - 1; i++) {
+    if (nums[i] > nums[i + 1]) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+/* prints an array in human-readable output to standard output */
+void print_arr(unsigned int *nums, unsigned int nums_len) {
+  printf("[\n");
+  for (unsigned int i = 0; i < nums_len; i++) {
+    printf("  %d", nums[i]);
+    if (i != nums_len - 1) {
+      putchar(',');
+    }
+    putchar('\n');
+  }
+  printf("]\n");
+}
+
+/* insertion sort on an array of unsigned ints */
+void insertion_sort(unsigned int *nums, unsigned int len_nums) {
+  for (unsigned int i = 1; i < len_nums; i++) {
+    unsigned int current_idx = i;
+    unsigned int temp = nums[i];
+    while (current_idx > 0 && temp < nums[current_idx - 1]) {
+      nums[current_idx] = nums[current_idx - 1];
+      current_idx--;
+    }
+    nums[current_idx] = temp;
+  }
+}
+
+int main() {
+  unsigned int *arr = (unsigned int *)malloc(ARRAY_LENGTH * sizeof(int));
+  srand(time(NULL));
+
+  for (unsigned int i = 0; i < ARRAY_LENGTH; i++) {
+    arr[i] = rand() % (65536);
+  }
+
+  print_arr(arr,ARRAY_LENGTH);
+  insertion_sort(arr, ARRAY_LENGTH);
+  print_arr(arr, ARRAY_LENGTH);
+
+  if (check_sorted(arr, ARRAY_LENGTH))
+    printf("Result of sorting check: True (selection sort worked)\n");
+  else
+    printf("Result of sorting check: False (selection sort failed)\n");
+
+  printf("Rand max: %d", RAND_MAX);
+}
