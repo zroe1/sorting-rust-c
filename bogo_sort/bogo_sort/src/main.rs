d1c1b7dc85c734077b6b2ab5f54db0b25be03e30
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-08-17 23:39:19 -0500

    fixed csv format

--- bogo_sort/bogo_sort/src/main.rs
+++ bogo_sort/bogo_sort/src/main.rs
@@ -42,7 +42,7 @@ fn bogo_sort(nums: &mut Vec<u16>) {
 
 fn main() {
     let mut rng = rand::thread_rng();
-
+    println!("n,t");
     for i in 0..10 {
         let mut nums: Vec<u16> = vec![0; i];
         for num in nums.iter_mut() {
