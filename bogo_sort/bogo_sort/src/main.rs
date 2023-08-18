5b756d788d77649564e33b305735e4c0f54c4f94
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-08-17 23:33:22 -0500

    created csv to storerust bogosort preformance data

--- bogo_sort/bogo_sort/src/main.rs
+++ bogo_sort/bogo_sort/src/main.rs
@@ -1,5 +1,6 @@
 extern crate rand;
 use rand::Rng;
+use std::time::{Instant};
 
 fn check_array_sorted(nums: &Vec<u16>) -> bool {
     let mut last_num: u16 = 0;
@@ -42,18 +43,15 @@ fn bogo_sort(nums: &mut Vec<u16>) {
 fn main() {
     let mut rng = rand::thread_rng();
 
-    let mut nums: Vec<u16> = vec![0; 10];
-    for num in nums.iter_mut() {
-        *num = rng.gen_range(0..u16::MAX);
-    }
+    for i in 0..10 {
+        let mut nums: Vec<u16> = vec![0; i];
+        for num in nums.iter_mut() {
+            *num = rng.gen_range(0..u16::MAX);
+        }
 
-    println!("{:#?}", nums);
-    bogo_sort(&mut nums);
-    println!("{:#?}", nums);
-
-    if check_array_sorted(&nums) {
-        println!("Result of sorting check: True (bogo sort worked)");
-    } else {
-        println!("Result of sorting check: False (bogo sort failed)");
+        let start = Instant::now();
+        bogo_sort(&mut nums);
+        let duration = start.elapsed();
+        println!("{i},{:?}", duration.as_secs_f64());
     }
 }
