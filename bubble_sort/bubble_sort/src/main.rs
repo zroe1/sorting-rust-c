48f8c614aede1b92ffe9325f163894c6a1eb252c
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-08-18 21:15:13 -0500

    added bubble sort data

--- bubble_sort/bubble_sort/src/main.rs
+++ bubble_sort/bubble_sort/src/main.rs
@@ -1,6 +1,8 @@
 extern crate rand;
 use rand::Rng;
+use std::time::Instant;
 
+// FUNCTION USED ONLY FOR CHECKING SOLUTIONS
 fn check_array_sorted(nums: &Vec<u16>) -> bool {
     let mut last_num: u16 = 0;
     for num in nums.iter() {
@@ -26,19 +28,16 @@ fn bubble_sort(nums: &mut Vec<u16>) {
 
 fn main() {
     let mut rng = rand::thread_rng();
+    println!("n,t");
+    for i in 0..300 {
+        let mut nums: Vec<u16> = vec![0; i * 100];
+        for num in nums.iter_mut() {
+            *num = rng.gen_range(0..u16::MAX);
+        }
 
-    let mut nums: Vec<u16> = vec![0; 10];
-    for num in nums.iter_mut() {
-        *num = rng.gen_range(0..u16::MAX);
-    }
-
-    println!("{:#?}", nums);
-    bubble_sort(&mut nums);
-    println!("{:#?}", nums);
-
-    if check_array_sorted(&nums) {
-        println!("Result of sorting check: True (insertion sort worked)");
-    } else {
-        println!("Result of sorting check: False (insertion sort failed)");
+        let start = Instant::now();
+        bubble_sort(&mut nums);
+        let duration = start.elapsed();
+        println!("{},{:?}", i * 100, duration.as_secs_f64());
     }
 }
